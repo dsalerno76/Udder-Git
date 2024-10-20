c8248cff08a3f5c85ffefe1e805537f0e822ac63
Davide Salerno <dsalerno76@gmail.com>
2024-10-20 15:41:50 +0200

    xxxxx

--- Udder-Git/ContentView.swift
+++ Udder-Git/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Hello, world!")
-            Text("Hello, world!")
+            Text("Hello, !")
             Text("Hello, world!")
         }
         .padding()
