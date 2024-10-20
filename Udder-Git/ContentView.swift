f15d8bdf61227309b47397ba1de3ccc35e8d7c6d
Davide Salerno <dsalerno76@gmail.com>
2024-10-20 15:29:36 +0200

    Initial Commit

---
+++ Udder-Git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udder-Git
+//
+//  Created by Davide Salerno on 20/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder-Git/Udder_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udder_GitApp.swift
+//  Udder-Git
+//
+//  Created by Davide Salerno on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Udder_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
